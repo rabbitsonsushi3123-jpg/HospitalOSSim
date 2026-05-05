ce3f1706cbf93de9e9df5ee84a9483e628c4efd3
Dianna Dominguez <rabbitsonsushi3123@gmail.com>
2026-05-05 12:59:06 -0700

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,6 +1,6 @@
 #include "Logger.h"
 #include <iostream>
 
-void Logger::log(const std::string& event) {
-    std::cout << "[LOG] " << event << std::endl;
+void Logger::log(const std::string& message) {
+    std::cout << message << std::endl;
 }
