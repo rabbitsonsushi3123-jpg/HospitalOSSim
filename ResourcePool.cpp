72191c1a8328e7a030469f5af1843cbb25b4e975
Dianna Dominguez <rabbitsonsushi3123@gmail.com>
2026-05-05 12:55:31 -0700

    Update ResourcePool.cpp

--- ResourcePool.cpp
+++ ResourcePool.cpp
@@ -1,5 +1,20 @@
 #include "ResourcePool.h"
 
-ResourcePool::ResourcePool(int count) {
-    resources.resize(count, true);
+ResourcePool::ResourcePool() {
+    resourceNames = {
+        "MRI Scanner",
+        "Blood Lab",
+        "Operating Room",
+        "X-Ray Machine"
+    };
+
+    available.resize(resourceNames.size(), true);
+}
+
+int ResourcePool::size() const {
+    return available.size();
+}
+
+std::string ResourcePool::getResourceName(int resourceId) const {
+    return resourceNames[resourceId];
 }
